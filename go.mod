c7bbe19adebb4a3be145873a9996d1d0b548a29c
Laszlo Lazuer <visualjazz@gmail.com>
2024-07-17 15:33:46 -0600

    its doing something.

--- go.mod
+++ go.mod
@@ -4,14 +4,20 @@ go 1.21
 
 toolchain go1.22.5
 
-require github.com/wailsapp/wails/v2 v2.9.1
+require (
+	github.com/minio/selfupdate v0.6.0
+	github.com/sanbornm/go-selfupdate v0.0.0-20230714125711-e1c03e3d6ac7
+	github.com/wailsapp/wails/v2 v2.9.1
+)
 
 require (
+	aead.dev/minisign v0.2.0 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/kr/binarydist v0.1.0 // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 
+	"github.com/sanbornm/go-selfupdate/selfupdate"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -10,6 +11,20 @@ import (
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+// the app's version. This will be set on build.
+var version string
+
+// go-selfupdate setup and config
+var updater = &selfupdate.Updater{
+	CurrentVersion: version,                  // Manually update the const, or set it using `go build -ldflags="-X main.VERSION=<newver>" -o hello-updater src/hello-updater/main.go`
+	ApiURL:         "https://laszlo-lazuer.github.io/artifactory/hello-updater/", // The server hosting `$CmdName/$GOOS-$ARCH.json` which contains the checksum for the binary
+	BinURL:         "http://localhost:8080/", // The server hosting the zip file containing the binary application which is a fallback for the patch method
+	DiffURL:        "http://localhost:8080/", // The server hosting the binary patch diff for incremental updates
+	Dir:            "update/",                // The directory created by the app when run which stores the cktime file
+	CmdName:        "basic",          // The app name which is appended to the ApiURL to look for an update
+	ForceCheck:     true,                     // For this example, always check for an update unless the version is "dev"
+}
 
 func main() {
 	// Create an instance of the app structure
